d43368670c8c9e4f6051fbc9b6c9ebcab3e87b11
alainkatayi <alainkatayi01@gmail.com>
2025-08-12 14:19:51 +0100

    Création de l'interface Experiences dans le fichier model profile.ts

--- src/app/core/models/profile.ts
+++ src/app/core/models/profile.ts
@@ -13,3 +13,14 @@ export interface Certifications{
     organisme:string
     obtaining_date:string
 }
+
+
+export interface Experiences{
+    id:number
+    job_title:string
+    entreprise_name :string
+    start_date :string
+    end_date:string
+    current_job:Boolean
+    description:Text
+}
