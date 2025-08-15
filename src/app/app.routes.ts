fdf088a2a7cf07704a2d307604ce727e77582772
alainkatayi <alainkatayi01@gmail.com>
2025-08-15 14:08:27 +0100

    Création du composant dashboard(statique), qui affiche les statistique et les actions rapides

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -26,5 +26,12 @@ export const routes: Routes = [
         loadComponent: () =>
             import('../app/pages/login/login.component').then((m) => m.LoginComponent),
     }
+    ,
+    {
+        path:'blog-aYdXmXiBn/dashboard',
+        title:'admin',
+        loadComponent: () =>
+            import('../app/pages/admin/dashboard/dashboard.component').then((m) => m.DashboardComponent),
+    }
 
 ];
---
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../components/header/header.component";
+import { AuthLoginResponse } from '../../../core/models/auth';
+import { UserLocalService } from '../../../core/services/userLocal/user-local.service';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+  user!:AuthLoginResponse | null
+  constructor(private userLocalService: UserLocalService ){}
+  
+  ngOnInit(){
+    this.user = this.userLocalService.getUser()
+    console.log("User",this.user)
+  }
+}
