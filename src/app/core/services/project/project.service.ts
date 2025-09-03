8a4cfa90e841d91cad6099402e5c5b8d2196a529
alainkatayi <alainkatayi01@gmail.com>
2025-09-03 13:48:24 +0100

    Ajout de méthode createProject dans le service project, qui permet de creer un projet via l'api

--- src/app/core/services/project/project.service.ts
+++ src/app/core/services/project/project.service.ts
@@ -3,15 +3,21 @@ import { Injectable } from '@angular/core';
 import { environnement } from '../../../../environnemnts/environnement';
 import { Observable } from 'rxjs';
 import { Project } from '../../models/project';
+import { UserLocalService } from '../userLocal/user-local.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
   private url = environnement.apiUrl
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient , private userLocalService:UserLocalService) { }
 
   getProjects():Observable<Project[]>{
     return this.http.get<Project[]>(`${this.url}project/project/`)
+  }
+
+  createProject(data:FormData){
+    const headers= this.userLocalService.getAuthHeaders()
+    return  this.http.post(this.url + 'project/create/', data, {headers})
   }
 }
