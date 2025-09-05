30c7ba73b4d063e9f8070d96e1a2d5734b5ffedd
alainkatayi <alainkatayi01@gmail.com>
2025-09-05 19:48:07 +0100

    Implémentation de la suppression d'une certification via le service profile dans le composant certification-list

--- src/app/pages/admin/certification-list/certification-list.component.ts
+++ src/app/pages/admin/certification-list/certification-list.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Certifications } from '../../../core/models/profile';
 import { ProfileService } from '../../../core/services/profiles/profile.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certification-list',
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './certification-list.component.html',
   styleUrl: './certification-list.component.css'
 })
 export class CertificationListComponent {
   certifications!:Certifications[]
-
+  certificationId: number = -1;
+  DeleteModalOpen = false;
   constructor(private profileService:ProfileService){}
 
 
@@ -29,6 +31,32 @@ export class CertificationListComponent {
         console.error("erreur lors de la récupération des certificats",error)
       }
     })
+  }
+
+    deleteSkill() {
+    this.profileService.deleteCertification(this.certificationId).subscribe({
+      next: (response) => {
+        window.location.reload();
+        console.log(response)
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
+  }
+
+  openDeleteModal(id: number) {
+    this.DeleteModalOpen = true;
+    this.certificationId = id;
+  }
+
+  confirmDelete() {
+    this.deleteSkill();
+    this.closeDeleteModal();
+  }
+
+  closeDeleteModal() {
+    this.DeleteModalOpen = false;
   }
 
 }
